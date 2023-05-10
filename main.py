7c3b7e9e0a7c0127f267c2842de7f15d2f5839cb
withmeT <withme00@163.com>
2023-05-10 22:11:25 +0800

    5.10

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def cli():         #设置参数
     subparser1 = subparsers.add_parser('linux', help='linux help')
     subparser1.add_argument('--host',dest='host', help='单主机采集（以”设备名~IP地址~用户名~密码~端口“）')
     subparser1.add_argument('--hosts',dest='hosts', help='多主机采集(default ./linux.txt)',default="linux.txt")
-    subparser1.add_argument('--common',dest='comm', help='多主机采集(以分号进行分割)')
+    subparser1.add_argument('--common',dest='comm', help='自定义命令采集(以分号进行分割)')
     subparser2 = subparsers.add_parser('mysql', help='mysql help')
     subparser2.set_defaults()
     return parser.parse_args()
