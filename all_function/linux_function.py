760769b46541f34de0f0551f927bc521beb5b56b
withmeT <withme00@163.com>
2023-05-10 22:56:36 +0800

    5.10

--- all_function/linux_function.py
+++ all_function/linux_function.py
@@ -5,12 +5,9 @@
 @File ：linux_function.py
 """
 import paramiko
-import datetime
-import time
 from currency import write_file
 class run_linux:
     def __init__(self,date_list):
-        t1 = time.time()
         file_name, host, username, password, port = date_list.split("~")
         ssh = paramiko.SSHClient()  # 建立连接
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -23,17 +20,10 @@ class run_linux:
             pass
         self.ssh = ssh
         self.file_name = "date/" + file_name + "+" + host + ".log"
-        t2 = time.time()
-        print(t2-t1)
     def run(self,cmd):
-        if self.con_state == True :
-            for i in cmd:
-                stdin, stdout, stderr = self.ssh.exec_command(i)
-                write_file(self.file_name,stdout.read().decode("utf-8"))
-        else :
-            error_date = "时间：" + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ",失败主机：" + i.split("~")[0] + "---" + i.split("~")[1] + "\n"
-            print(error_date)
-            write_file("error.log",error_date)
+        for i in cmd:
+            stdin, stdout, stderr = self.ssh.exec_command(i)
+            write_file(self.file_name,stdout.read().decode("utf-8"))
     def __call__(self, cmd, *args, **kwargs):
         return self.run(cmd)
 
--- all_function/threads.py
+++ all_function/threads.py
@@ -12,18 +12,21 @@ import time
 from all_function.linux_function import run_linux
 from currency import *
 
-def start_threads(date,cmd):
-    run_linux(date).run(cmd=cmd)
-
 def linux_threads_pool(file_name,cmd):
     if file_exists(file_name):
-        new_date = []
-        for i in read_file(file_name):  # 获取资产内容，并进行采集
-            new_date.append(i)
-        pool_num = len(new_date)
+        pool_num = len(read_file(file_name))
         pool = ThreadPoolExecutor(max_workers=pool_num)
-        for i in new_date:  # 获取资产内容，并进行采集
-            pool.submit(lambda cxp:start_threads(*cxp).run,(i,cmd))
+        for i in read_file(file_name):  # 获取资产内容，并进行采集
+            if check_in_date(i):
+                print(i)
+                # pool.submit(lambda cxp:run(*cxp),(i ,cmd))
+                linux_con = run_linux(i)
+                if linux_con.con_state == True:  # 判断是否成功建立连接，若失败返回信息
+                    pool.submit(linux_con.run, cmd)
+                else:
+                    error_date = "时间：" + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ",失败主机：" + i.split("~")[0] + "---" + i.split("~")[1] + "\n"
+                    print(error_date)
+                    write_file("error.log",error_date)
         time.sleep(1)  # 不加延迟，会导致线程结果无法返回
     else:
         write_file("linux.txt", "名称~IP地址~用户名~密码~端口")
