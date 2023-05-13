12c293ecc1ac33e87ac348a73f2c4df36952af89
withmeT <withme00@163.com>
2023-05-13 22:08:59 +0800

    5.12

--- all_function/linux_function.py
+++ all_function/linux_function.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import paramiko
+import datetime
 from currency import write_file
 
 
@@ -10,7 +11,7 @@ class run_linux:
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         try:
             ssh.connect(hostname=host, port=port, username=username, password=password, allow_agent=False,
-                        look_for_keys=False, timeout=3)
+                        look_for_keys=False, timeout=5)
             self.con_state = True  # 连接状态码
         except:
             self.con_state = False
@@ -24,3 +25,14 @@ class run_linux:
 
     def __call__(self, cmd, *args, **kwargs):
         return self.run(cmd)
+
+
+def run(data, cmd):
+    linux_con = run_linux(data)
+    if linux_con.con_state:  # 判断是否成功建立连接，若失败返回信息
+        linux_con.run(cmd)
+    else:
+        error_date = "时间：" + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ",失败主机：" + \
+                     data.split("~")[0] + "---" + data.split("~")[1] + "\n"
+        print(error_date)
+        write_file("error.log", error_date)
--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import argparse
+import time
+
 from all_function.threads import *
+from all_function.linux_function import run_linux
 import yaml
 
 if not os.path.exists("date"):  # 判断是否存在存储采集结果的目录
     os.makedirs("date")
 
-with open("default.yaml", "r", encoding="utf8")as f:        # 读取本地默认参数
+with open("default.yaml", "r", encoding="utf8")as f:  # 读取本地默认参数
     cli_yaml = yaml.safe_load(f)
+
 
 def cli():  # 设置参数
     parser = argparse.ArgumentParser(description='等保测评工具')
     subparsers = parser.add_subparsers(dest="command")
     subparser1 = subparsers.add_parser('linux', help='linux help')
     subparser1.add_argument('--host', dest='host', help='单主机采集（以”设备名~IP地址~用户名~密码~端口“）')
-    subparser1.add_argument('--hosts', dest='hosts', help='多主机采集(default ./linux.txt)', default=cli_yaml.get("linux-hosts"))
+    subparser1.add_argument('--hosts', dest='hosts', help='多主机采集(default ./linux.txt)',
+                            default=cli_yaml.get("linux-hosts"))
     subparser1.add_argument('--common', dest='comm', help='自定义命令采集(以分号进行分割)')
     subparser2 = subparsers.add_parser('mysql', help='mysql help')
     subparser2.set_defaults()
@@ -25,10 +30,10 @@ if __name__ == '__main__':  # 主函数
     if args.command == 'linux':  # linux模式
         with open("cmd.yaml", "r", encoding="utf8")as f:
             cmd = yaml.safe_load(f).get("linux_cmd")
-        if args.comm:
-            cmd = args.comm.split(";")
-        if args.host:  # 判断主机模式
+        if args.comm:  # 判断是否自定义命令
+            cmd = args.comm
+        if args.host:  # 主机类型为单主机采集
             if check_in_data(args.host):
                 run_linux(args.host).run(cmd=cmd)
-        elif args.hosts:
+        elif args.hosts:  # 主机类型为多主机采集
             linux_threads_pool(args.hosts, cmd=cmd)
